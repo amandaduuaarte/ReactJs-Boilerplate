3d9b654d39e5d5fb134082d6367acd9460ef475f
amandaduuaarte <amandaduuaarte@gmail.com>
2023-07-02 18:40:21 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        "plugin:react/recommended",
+        "airbnb",
+        "plugin:prettier/recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        babelOptions: {
+            presets: ["@babel/preset-react"],
+        },
+        ecmaVersion: "latest",
+        requireConfigFile: false,
+        sourceType: "module",
+    },
+    plugins: ["react", "prettier", "react-hooks", "@typescript-eslint"],
+    rules: {
+        "prettier/prettier": 2,
+        "react/jsx-filename-extension": 0,
+        "import/prefer-default-export": 0,
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+        "import/extensions": 0,
+    },
+    settings: {
+        "import/resolver": {
+            node: {
+                extensions: [".js", ".ts", ".jsx", ".tsx"],
+            },
+        },
+    },
+};
