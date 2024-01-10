ad1428f640aff4a911e7ccc539a5040303ce9be7
Amanda Duarte <amandaduuaarte@gmail.com>
2024-01-10 12:18:02 -0300

    refactor: Improving commit settings

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,23 @@
 module.exports = {
     extends: ["@commitlint/config-conventional"],
     rules: {
-        // Adicione suas regras personalizadas aqui
-        "type-enum": [2, "always", ["feat", "fix", "docs", "chore", "test"]],
+        "type-enum": [
+            2,
+            "always",
+            [
+                "build",
+                "chore",
+                "ci",
+                "docs",
+                "feat",
+                "fix",
+                "perf",
+                "refactor",
+                "revert",
+                "style",
+                "test",
+            ],
+        ],
         "subject-case": [2, "always", "sentence-case"],
         "subject-max-length": [2, "always", 100],
     },
